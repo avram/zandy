930b85eeb9a73aac803bf6599d9d4f92184d247f
Avram Lyon <avram@scopely.com>
2014-02-03 01:51:23 -0800

    rm incorrect copyright

--- src/main/java/com/gimranov/zandy/app/Application.java
+++ src/main/java/com/gimranov/zandy/app/Application.java
@@ -2,10 +2,6 @@ package com.gimranov.zandy.app;
 
 import com.squareup.otto.Bus;
 
-/**
- * Part of the Scopely™ Platform
- * © 2013 Scopely, Inc.
- */
 public class Application extends android.app.Application {
     private static final String TAG = Application.class.getCanonicalName();
 
