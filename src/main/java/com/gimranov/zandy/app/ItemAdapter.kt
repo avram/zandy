5d64d91e8d5591c8093c444701e006b6e2b5b501
Avram Lyon <avram@scopely.com>
2017-10-23 22:36:30 -0700

    Add data binding and fake data gen

--- src/main/java/com/gimranov/zandy/app/ItemAdapter.kt
+++ src/main/java/com/gimranov/zandy/app/ItemAdapter.kt
@@ -4,23 +4,48 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import com.gimranov.zandy.app.data.Item
+import com.gimranov.zandy.app.databinding.ItemCardBinding
 
-class ItemAdapter : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent!!.context).inflate(R.layout.item_card, parent, false)
-        return ViewHolder(view)
+class ItemAdapter : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent!!.context);
+        val cardBinding = ItemCardBinding.inflate(layoutInflater, parent, false)
+        return ItemViewHolder(cardBinding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        holder?.title?.text = "Aha! $position"
+    override fun onBindViewHolder(holder: ItemViewHolder?, position: Int) {
+        holder?.let {
+            it.binding.cardHeader.setOnClickListener { holder.toggle() }
+            it.bind(FakeDataUtil.book())
+        }
     }
 
     override fun getItemCount(): Int {
         return 100
     }
 
-    class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
-        val title = itemView?.findViewById<TextView>(R.id.card_item_title)
+
+    class ItemViewHolder(cardBinding: ItemCardBinding) : RecyclerView.ViewHolder(cardBinding.root) {
+        val binding = cardBinding
+        private var expanded = false
+
+        fun toggle() {
+            expanded = !expanded
+
+            if (expanded) {
+                binding.cardExpandedContent.visibility = View.GONE
+                binding.cardButtonBar.visibility = View.GONE
+            } else {
+                binding.cardExpandedContent.visibility = View.VISIBLE
+                binding.cardButtonBar.visibility = View.VISIBLE
+            }
+        }
+
+        fun bind(item : Item) {
+            binding.item = item
+            binding.executePendingBindings()
+        }
+
     }
 }
